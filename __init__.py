0937bc3c2ab56872e440e6899df0a7a3f6172286
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-11-27 22:50:20 +0100

    pygal graph added

--- __init__.py
+++ __init__.py
@@ -19,6 +19,7 @@ import gc
 from content_management import Content
 import smtplib
 from flask_mail import Mail, Message
+import pygal
 
 app = Flask(__name__, instance_path='')
 # app.config.update(
@@ -325,6 +326,22 @@ def interactive():
         return render_template("interactive.html")
     except Exception as e:
         return (str(e))
+
+@app.route('/pygalexample/')
+def pygalexample():
+    try:
+        graph = pygal.Line()
+        graph.title = '% Change Coolness of programming languages over time.'
+        graph.x_labels = ['2011', '2012', '2013', '2014', '2015', '2016']
+        graph.add('Python', [15, 31, 89, 200, 356, 900])
+        graph.add('Java', [15, 45, 76, 80, 91, 95])
+        graph.add('C++', [5, 51, 54, 102, 150, 201])
+        graph.add('All others combined!', [5, 15, 21, 55, 92, 105])
+        graph_data = graph.render_data_uri()
+        return render_template("graphing.html", graph_data = graph_data)
+    except Exception as e:
+        return (str(e))
+
 
 
 @app.route('/jinjaman/')
